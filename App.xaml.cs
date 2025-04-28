b6157bf0bed043e798409756c996abf26d1e6428
MatiasScott <mvaldivieso31@gmail.com>
2025-04-27 20:53:46 -0500

    eliminacion de paginas y cambio de metodo en app.xaml

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new Views.login());
+            return new Window(new NavigationPage(new Views.login()));
         }
     }
 }
